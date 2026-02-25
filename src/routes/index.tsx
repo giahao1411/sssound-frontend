3b0b788533755726f3186082ab9087848fee5751
giahao1411 <trangiahao498@gmail.com>
2026-02-25 14:43:17 +0700

    refactor(routes): small fix

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,7 @@ import HomePage from "@/pages/home";
 import LoginPage from "@/pages/login";
 import NotificationPage from "@/pages/notification";
 import TestPage from "@/pages/test";
-import type { AppRoute } from "@/types/app";
+import type { AppRoute } from "@/types";
 
 export const appRoutes: AppRoute[] = [
     { path: "/login", element: <LoginPage />, layout: "auth" },
